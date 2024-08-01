622bc2ab415f773182a1986d9408bc4442e5e067
Anand Shaurya <shaurya252anand@gmail.com>
2024-08-01 23:27:34 +0530

    Changed the List Groups.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,18 @@
 function ListGroup() {
-  const cities = ["Delhi", "Mumbai", "Bengaluru", "Dhanbad", "Hyderabad"];
+  let cities = ["Delhi", "Mumbai", "Bengaluru", "Dhanbad", "Hyderabad"];
 
   return (
     <>
       <h1>List</h1>
+      {/* {cities.length === 0 ? <p>No item found!</p> : null} */}
+      {cities.length === 0 && <p>No item found!</p>}
       <ul className="list-group">
         {cities.map((item) => (
-          <li key={item} className="list-group-item">
+          <li
+            key={item}
+            className="list-group-item"
+            onClick={() => console.log(item)}
+          >
             {item}
           </li>
         ))}
